5bca6c641a3def1b4c01a22b5f2e00f563454f8e
Prof. Libório Júnior <prof@liboriojunior.eti.br>
2020-08-25 22:30:41 -0300

    Aula 19/08/2020

---
+++ tas-core/src/main/java/br/edu/materdei/tas/core/entity/GrupoEntity.java
@@ -0,0 +1,49 @@
+package br.edu.materdei.tas.core.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "grupo")
+public class GrupoEntity {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    @Column(length = 30, nullable = false)
+    private String nome;
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+    
+}
