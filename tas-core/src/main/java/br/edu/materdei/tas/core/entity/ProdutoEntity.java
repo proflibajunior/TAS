2c036587647b69fb1e1df46fb2f84e3a4371b884
Prof. Libório Júnior <prof@liboriojunior.eti.br>
2020-08-26 20:26:46 -0300

    Aula 26/08/2020

---
+++ tas-core/src/main/java/br/edu/materdei/tas/core/entity/ProdutoEntity.java
@@ -0,0 +1,142 @@
+package br.edu.materdei.tas.core.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class ProdutoEntity {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    @Column(length = 6, nullable = false)
+    private String codigo;
+
+    @Column(length = 50, nullable = false)
+    private String nome;
+    
+    @Lob
+    @Column(length = 5000)
+    private String descricao;
+    
+    @Column(nullable = false)
+    private Double preco;
+    
+    @Column(length = 1, nullable = false)
+    private String ativo;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false)
+    private GrupoEntity grupo;
+
+    public ProdutoEntity() {
+        this.ativo = "S";
+    }
+    
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the preco
+     */
+    public Double getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    /**
+     * @return the ativo
+     */
+    public String getAtivo() {
+        return ativo;
+    }
+
+    /**
+     * @param ativo the ativo to set
+     */
+    public void setAtivo(String ativo) {
+        this.ativo = ativo;
+    }
+
+    /**
+     * @return the grupo
+     */
+    public GrupoEntity getGrupo() {
+        return grupo;
+    }
+
+    /**
+     * @param grupo the grupo to set
+     */
+    public void setGrupo(GrupoEntity grupo) {
+        this.grupo = grupo;
+    }
+
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+}
