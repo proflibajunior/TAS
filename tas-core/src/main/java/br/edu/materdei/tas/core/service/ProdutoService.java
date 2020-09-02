b1e3a308c665f77ea7e66b740354b65359bf2386
Prof. Libório Júnior <prof@liboriojunior.eti.br>
2020-09-02 20:16:13 -0300

    Aula 02/09/2020

--- tas-core/src/main/java/br/edu/materdei/tas/core/service/ProdutoService.java
+++ tas-core/src/main/java/br/edu/materdei/tas/core/service/ProdutoService.java
@@ -1,35 +1,35 @@
 package br.edu.materdei.tas.core.service;
 
-import br.edu.materdei.tas.core.entity.GrupoEntity;
+import br.edu.materdei.tas.core.entity.ProdutoEntity;
 import br.edu.materdei.tas.core.exception.ResourceNotFoundException;
-import br.edu.materdei.tas.core.repository.GrupoRepository;
+import br.edu.materdei.tas.core.repository.ProdutoRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class GrupoService implements IBaseService<GrupoEntity>{
+public class ProdutoService implements IBaseService<ProdutoEntity>{
 
     @Autowired
-    private GrupoRepository repository;
+    private ProdutoRepository repository;
     
     @Override
     @Transactional
-    public List<GrupoEntity> findAll() {
+    public List<ProdutoEntity> findAll() {
         return repository.findAll();
     }
 
     @Override
     @Transactional
-    public GrupoEntity findById(Integer id) throws ResourceNotFoundException {
+    public ProdutoEntity findById(Integer id) throws ResourceNotFoundException {
         return repository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException(id));
     }
 
     @Override
     @Transactional
-    public GrupoEntity save(GrupoEntity entity) {
+    public ProdutoEntity save(ProdutoEntity entity) {
         return repository.saveAndFlush(entity);
     }
 
