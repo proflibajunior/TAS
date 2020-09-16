cde0f23b21bf484ff7e9d03a4bd56f7d07797a3d
Prof. Libório Júnior <prof@liboriojunior.eti.br>
2020-09-16 20:28:30 -0300

    Aula 16/09/2020

--- tas-estoque/src/main/java/br/edu/materdei/tas/estoque/entity/EstoqueEntity.java
+++ tas-estoque/src/main/java/br/edu/materdei/tas/estoque/entity/EstoqueEntity.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -16,7 +17,7 @@ import javax.persistence.TemporalType;
 @Table(name = "estoque")
 public class EstoqueEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Integer id;
     
     @Temporal(TemporalType.DATE)
