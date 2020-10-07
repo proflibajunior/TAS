aa4d5a5938a74b681558560c3e5a30ac97e767a8
Prof. Libório Júnior <prof@liboriojunior.eti.br>
2020-10-07 19:56:12 -0300

    Aula 07/10/2020

--- tas-estoque/src/main/java/br/edu/materdei/tas/estoque/aspect/EstoqueAspect.java
+++ tas-estoque/src/main/java/br/edu/materdei/tas/estoque/aspect/EstoqueAspect.java
@@ -1,17 +1,37 @@
 package br.edu.materdei.tas.estoque.aspect;
 
+import br.edu.materdei.tas.compra.entity.CompraEntity;
+import br.edu.materdei.tas.estoque.entity.EstoqueEntity;
+import br.edu.materdei.tas.estoque.service.EstoqueService;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class EstoqueAspect {
     
+    @Autowired 
+    private EstoqueService service;
+    
     @AfterReturning(pointcut = "execution( * br.edu.materdei.tas.compra.service.CompraService.save(..))")
     public void salvarCompra(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        CompraEntity compra = (CompraEntity) args[0];
         
+        compra.getItens().forEach(item -> {
+            EstoqueEntity estoque = new EstoqueEntity();
+            
+            //Alimenta o estoque com os dados dos itens da nota
+            estoque.setProduto(item.getProduto());
+            estoque.setQtdade(item.getQtdade());
+            estoque.setHistorico("Movto de entrada originado pela compra "+ compra.getCodigo());
+            
+            //Grava a movimentação de estoque
+            this.service.save(estoque);
+        });
     }
     
     public void salvarVenda() {
