447ad5795a950a68988cd4f357eeab77658befc3
Prof. Libório Júnior <prof@liboriojunior.eti.br>
2020-10-07 20:27:28 -0300

    Aula 07/10/2020

--- tas-estoque/src/main/java/br/edu/materdei/tas/estoque/aspect/EstoqueAspect.java
+++ tas-estoque/src/main/java/br/edu/materdei/tas/estoque/aspect/EstoqueAspect.java
@@ -3,6 +3,7 @@ package br.edu.materdei.tas.estoque.aspect;
 import br.edu.materdei.tas.compra.entity.CompraEntity;
 import br.edu.materdei.tas.estoque.entity.EstoqueEntity;
 import br.edu.materdei.tas.estoque.service.EstoqueService;
+import br.edu.materdei.tas.venda.entity.VendaEntity;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
@@ -34,10 +35,31 @@ public class EstoqueAspect {
         });
     }
     
-    public void salvarVenda() {
+    @AfterReturning(pointcut = "execution( * br.edu.materdei.tas.venda.service.VendaService.save(..))")
+    public void salvarVenda(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        VendaEntity venda = (VendaEntity) args[0];
+        
+        venda.getPedido().getItens().forEach(item -> {
+            EstoqueEntity estoque = new EstoqueEntity();
+            
+            //Alimenta o estoque com os dados dos itens da nota
+            estoque.setProduto(item.getProduto());
+            estoque.setQtdade(item.getQtdade()*-1);
+            estoque.setHistorico("Movto de saída originado pela venda "+ venda.getCodigo());
+            
+            //Grava a movimentação de estoque
+            this.service.save(estoque);
+        });
     }
     
-    public void excluirCompra() {
+    @AfterReturning(pointcut = "execution( * br.edu.materdei.tas.compra.service.CompraService.delete(..))")
+    public void excluirCompra(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        Integer id = (Integer) args[0];
+        
+        
+        
     }
     
     public void excluirVenda() {
