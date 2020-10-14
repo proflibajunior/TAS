aaf91a2c7739871d9596f3f931560a1683ce6a18
Prof. Libório Júnior <prof@liboriojunior.eti.br>
2020-10-14 11:14:11 -0300

    Aula 07/10/2020

--- tas-estoque/src/main/java/br/edu/materdei/tas/estoque/aspect/EstoqueAspect.java
+++ tas-estoque/src/main/java/br/edu/materdei/tas/estoque/aspect/EstoqueAspect.java
@@ -1,12 +1,18 @@
 package br.edu.materdei.tas.estoque.aspect;
 
 import br.edu.materdei.tas.compra.entity.CompraEntity;
+import br.edu.materdei.tas.compra.service.CompraService;
+import br.edu.materdei.tas.core.exception.ResourceNotFoundException;
 import br.edu.materdei.tas.estoque.entity.EstoqueEntity;
 import br.edu.materdei.tas.estoque.service.EstoqueService;
 import br.edu.materdei.tas.venda.entity.VendaEntity;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +23,12 @@ public class EstoqueAspect {
     @Autowired 
     private EstoqueService service;
     
+    @Autowired
+    private CompraService compraService;
+    
+    @Autowired
+    private EntityManager entityManager;
+    
     @AfterReturning(pointcut = "execution( * br.edu.materdei.tas.compra.service.CompraService.save(..))")
     public void salvarCompra(JoinPoint joinPoint) {
         Object[] args = joinPoint.getArgs();
@@ -25,7 +37,7 @@ public class EstoqueAspect {
         compra.getItens().forEach(item -> {
             EstoqueEntity estoque = new EstoqueEntity();
             
-            //Alimenta o estoque com os dados dos itens da nota
+            //Alimenta o estoque com os dados dos itens da comra
             estoque.setProduto(item.getProduto());
             estoque.setQtdade(item.getQtdade());
             estoque.setHistorico("Movto de entrada originado pela compra "+ compra.getCodigo());
@@ -43,7 +55,7 @@ public class EstoqueAspect {
         venda.getPedido().getItens().forEach(item -> {
             EstoqueEntity estoque = new EstoqueEntity();
             
-            //Alimenta o estoque com os dados dos itens da nota
+            //Alimenta o estoque com os dados dos itens da venda
             estoque.setProduto(item.getProduto());
             estoque.setQtdade(item.getQtdade()*-1);
             estoque.setHistorico("Movto de saída originado pela venda "+ venda.getCodigo());
@@ -53,16 +65,53 @@ public class EstoqueAspect {
         });
     }
     
-    @AfterReturning(pointcut = "execution( * br.edu.materdei.tas.compra.service.CompraService.delete(..))")
+    @Before("execution( * br.edu.materdei.tas.compra.service.CompraService.delete(..))")
     public void excluirCompra(JoinPoint joinPoint) {
         Object[] args = joinPoint.getArgs();
         Integer id = (Integer) args[0];
         
-        
-        
+        try {
+            /**
+             * FindById: Utiliza o EntityManager.find, ou seja, busca no banco de dados
+             * Neste caso colocamos o @Before, pois AfterReturn ocorre depois da exclusão, 
+             * ou seja depois que o registro já não existe.
+             */
+            CompraEntity compra = this.compraService.findById(id);
+            
+            compra.getItens().forEach(item -> {
+                
+                EstoqueEntity estoque = new EstoqueEntity();
+                
+                //Alimento o estoque com os dados dos itens da compra, fazendo um estorno
+                estoque.setProduto(item.getProduto());
+                estoque.setQtdade(item.getQtdade()*-1);
+                estoque.setHistorico("Movto de saída originado pelo estorno da compra "+ compra.getCodigo());
+                
+                this.service.save(estoque);
+            
+            });
+            
+        } catch (ResourceNotFoundException ex) {
+            Logger.getLogger(EstoqueAspect.class.getName()).log(Level.SEVERE, null, ex);
+        }   
     }
     
-    public void excluirVenda() {
+    @AfterReturning(pointcut = "execution(* br.edu.materdei.tas.venda.service.VendaService.delete(..))")
+    public void estornarVenda(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();        
+        Integer id = (Integer) args[0];
+        
+        VendaEntity venda = entityManager.getReference(VendaEntity.class, id);
+
+        venda.getPedido().getItens().forEach(item -> {
+
+            EstoqueEntity estoque = new EstoqueEntity();
+            estoque.setProduto(item.getProduto());
+            estoque.setQtdade(item.getQtdade());
+            estoque.setHistorico("Movimento de entrada originado pelo estorno da venda "+ venda.getCodigo());
+
+            service.save(estoque);
+        });
     }
         
     
